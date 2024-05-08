55d1cdef7d6dfc891c8ca11d5d14672d66b7d7ae
BSTluo <1946831552@qq.com>
2024-05-08 09:47:39 +0800

    更新欢迎词和送别词的设定

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,28 @@
-import { Context, Schema, Random, h } from 'koishi';
+import { Context, Schema, Random, h, Session } from 'koishi';
 import { } from 'koishi-plugin-adapter-iirose';
 
 export const name = 'iirose-welcome';
 
-export interface Config {
+export interface Config
+{
   fit: boolean;
   private: boolean;
   welcomeList: string[];
   exitList: string[];
+}
+
+declare module 'koishi' {
+  interface Tables
+  {
+    iirose_welcome: iirose_welcome;
+  }
+}
+
+export interface iirose_welcome
+{
+  uid: string;
+  welcomeMsg: string;
+  leaveMsg: string;
 }
 
 export const Config: Schema<Config> = Schema.intersect([
@@ -21,67 +36,175 @@ export const Config: Schema<Config> = Schema.intersect([
   }).description('欢迎词配置')
 ]);
 
-const getWelcome = (config: Config, username: string) => {
-  const random = new Random(() => Math.random())
-  const index = random.int(0, config.welcomeList.length);
-  
-  const welcomeMsg = config.welcomeList[index];
+export function apply(ctx: Context, config: Config)
+{
+  const getWelcome = async (config: Config, username: string, session: Session) =>
+  {
+    const getMsgTemp = await ctx.database.get('iirose_welcome', { uid: session.userId });
 
-  const msg = (/@/.test(welcomeMsg)) ? welcomeMsg.replace('(@)', `<at name="${username}"/>`) : welcomeMsg;
-  return msg;
-};
+    const getMsg = getMsgTemp.length > 0 ? getMsgTemp[0].leaveMsg : null;
 
-const getExit = (config: Config, username: string) => {
-  const random = new Random(() => Math.random())
-  const index = random.int(0, config.exitList.length);
+    if (getMsg && getMsg != '')
+    {
+      const msg = (/@/.test(getMsg)) ? getMsg.replace('(@)', `<at name="${username}"/>`) : getMsg;
 
-  const exitMsg = config.exitList[index];
-  
-  const msg = (/@/.test(exitMsg)) ? exitMsg.replace('(@)', `<at name="${username}"/>`) : exitMsg;
-  return msg;
-};
+      return msg;
+    }
 
-export function apply(ctx: Context, config: Config) {
-  ctx.on('iirose/joinRoom', (session) => {
-    if (config.welcomeList.length <= 0) { return }
-    
-    if (!config.private) {
-      session.bot.sendMessage('public:',getWelcome(config, session.username))
-    } else {
-      session.bot.sendMessage(`private:${session.userId}`, getWelcome(config, session.username));
+    const random = new Random(() => Math.random());
+    const index = random.int(0, config.welcomeList.length);
+
+    const welcomeMsg = config.welcomeList[index];
+
+    const msg = (/@/.test(welcomeMsg)) ? welcomeMsg.replace('(@)', `<at name="${username}"/>`) : welcomeMsg;
+
+    return msg;
+  };
+
+  const getExit = async (config: Config, username: string, session: Session) =>
+  {
+    const getMsgTemp = await ctx.database.get('iirose_welcome', { uid: session.userId });
+
+    const getMsg = getMsgTemp.length > 0 ? getMsgTemp[0].welcomeMsg : null;
+
+    if (getMsg && getMsg != '')
+    {
+      const msg = (/@/.test(getMsg)) ? getMsg.replace('(@)', `<at name="${username}"/>`) : getMsg;
+
+      return msg;
+    }
+
+    const random = new Random(() => Math.random());
+    const index = random.int(0, config.exitList.length);
+
+    const exitMsg = config.exitList[index];
+
+    const msg = (/@/.test(exitMsg)) ? exitMsg.replace('(@)', `<at name="${username}"/>`) : exitMsg;
+    return msg;
+  };
+
+  ctx.model.extend('iirose_welcome', {
+    uid: 'string',
+    welcomeMsg: 'string',
+    leaveMsg: 'string'
+  }, {
+    primary: 'uid'
+  });
+
+  ctx.on('iirose/joinRoom', async (session) =>
+  {
+    if (config.welcomeList.length <= 0) { return; }
+    if (session.userId == session.bot.user.id || session.userId == session.bot.selfId) { return; }
+
+    if (!config.private)
+    {
+      session.bot.sendMessage('public:', await getWelcome(config, session.username, session));
+    } else
+    {
+      session.bot.sendMessage(`private:${session.userId}`, await getWelcome(config, session.username, session));
     }
   });
 
-  ctx.on('iirose/leaveRoom', (session) => {
-    if (config.exitList.length <= 0) { return }
-    
-    if (!config.private) {
-      session.bot.sendMessage('public:',getExit(config, session.username))
-    } else {
-      session.bot.sendMessage(`private:${session.userId}`, getExit(config, session.username));
+  ctx.on('iirose/leaveRoom', async (session) =>
+  {
+    if (config.exitList.length <= 0) { return; }
+    if (session.userId == session.bot.user.id || session.userId == session.bot.selfId) { return; }
+
+    if (!config.private)
+    {
+      session.bot.sendMessage('public:', await getExit(config, session.username, session));
+    } else
+    {
+      session.bot.sendMessage(`private:${session.userId}`, await getExit(config, session.username, session));
     }
 
   });
 
-  if (config.fit) {
-    ctx.on('guild-member-added', session => {
-      if (config.welcomeList.length <= 0) { return }
+  if (config.fit)
+  {
+    ctx.on('guild-member-added', async session =>
+    {
+      if (config.welcomeList.length <= 0) { return; }
+      if (session.userId == session.bot.user.id || session.userId == session.bot.selfId) { return; }
 
-      if (!config.private) {
-        session.send(getWelcome(config, session.username));
-      } else {
-        session.bot.sendPrivateMessage(session.channelId, getWelcome(config, session.username));
+      if (!config.private)
+      {
+        session.send(await getWelcome(config, session.username, session));
+      } else
+      {
+        session.bot.sendPrivateMessage(session.channelId, await getWelcome(config, session.username, session));
       }
     });
 
-    ctx.on('guild-member-removed', session => {
-      if (config.exitList.length <= 0) { return }
-    
-      if (!config.private) {
-        session.send(getExit(config, session.username));
-      } else {
-        session.bot.sendPrivateMessage(session.channelId, getExit(config, session.username));
+    ctx.on('guild-member-removed', async session =>
+    {
+      if (config.exitList.length <= 0) { return; }
+      if (session.userId == session.bot.user.id || session.userId == session.bot.selfId) { return; }
+
+      if (!config.private)
+      {
+        session.send(await getExit(config, session.username, session));
+      } else
+      {
+        session.bot.sendPrivateMessage(session.channelId, await getExit(config, session.username, session));
       }
     });
   }
+
+  ctx.command('iirose', '花园工具');
+
+  ctx.command("iirose.wb.set <message>", '设置自己的专属欢迎词').action(async (v, message) =>
+  {
+    if (v.session.platform != 'iirose') { return; }
+    if (!message) { return `<at name="${v.session.username}"/>你没有设置欢迎词`; }
+
+    await ctx.database.upsert('iirose_welcome', [{
+      uid: v.session.userId,
+      welcomeMsg: message
+    }]);
+
+    return `<at name="${v.session.username}"/>设置成功`;
+  });
+
+  ctx.command("iirose.wb.rm", '清除自己设置的专属欢迎词').action(async v =>
+  {
+    if (v.session.platform != 'iirose') { return; }
+    const userDataTemp = await ctx.database.get('iirose_welcome', v.session.userId);
+    if (userDataTemp.length <= 0) { return `<at name="${v.session.username}"/>你没有设置自己的欢迎词`; }
+
+    await ctx.database.upsert('iirose_welcome', [{
+      uid: v.session.userId,
+      welcomeMsg: ''
+    }]);
+
+    return `<at name="${v.session.username}"/>删除成功`;
+  });
+
+  ctx.command("iirose.lr.set <message>", '设置自己的专属送别词').action(async (v, message) =>
+  {
+    if (v.session.platform != 'iirose') { return; }
+    if (!message) { return `<at name="${v.session.username}"/>你没有设置欢迎词`; }
+
+    await ctx.database.upsert('iirose_welcome', [{
+      uid: v.session.userId,
+      leaveMsg: message
+    }]);
+
+    return `<at name="${v.session.username}"/>设置成功`;
+  });
+
+  ctx.command("iirose.lr.rm", '清除自己设置的专属送别词').action(async v =>
+  {
+    if (v.session.platform != 'iirose') { return; }
+    const userDataTemp = await ctx.database.get('iirose_welcome', v.session.userId);
+    if (userDataTemp.length <= 0) { return `<at name="${v.session.username}"/>你没有设置自己的欢迎词`; }
+
+    await ctx.database.upsert('iirose_welcome', [{
+      uid: v.session.userId,
+      leaveMsg: ''
+    }]);
+
+    return `<at name="${v.session.username}"/>删除成功`;
+  });
+
 }
